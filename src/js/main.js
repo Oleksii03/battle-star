384b110067659e1e3f793867595a59921865d3c2
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-08-22 14:04:24 +0300

    auto commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,1 @@
-import { createMobileMenu } from './partials/mobile-menu';
-import { createRoadmap } from './partials/roadmap';
-import { createAccordionFaq } from './partials/accordion-faq';
-import { footer } from './partials/footer';
-
-document.addEventListener('DOMContentLoaded', () => {
-  createMobileMenu();
-  createRoadmap();
-  createAccordionFaq();
-  footer();
-});
+// document.addEventListener('DOMContentLoaded', () => {});
