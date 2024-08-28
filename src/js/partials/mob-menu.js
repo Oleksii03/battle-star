d97164e0fb481e2beedb1893f1c089d29e7d5f77
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-08-28 13:10:40 +0300

    add

--- src/js/partials/mob-menu.js
+++ src/js/partials/mob-menu.js
@@ -4,6 +4,10 @@ export function mobMenu() {
   const closeMenuBtn = document.querySelector('.js-mob-menu-btn-close');
   const backdrop = document.querySelector('.js-backdrop');
 
+  const chooseCity = document.querySelector('.js-choose-city');
+  const citySelectionList = document.querySelector('.js-city-selection-list');
+  const activeCity = document.querySelector('.js-active-city');
+
   openMenuBtn.addEventListener('click', toogleMenu);
   closeMenuBtn.addEventListener('click', toogleMenu);
   backdrop.addEventListener('click', toogleMenu);
@@ -11,5 +15,29 @@ export function mobMenu() {
   function toogleMenu(e) {
     mobMenu.classList.toggle('mob-menu--visible');
     backdrop.classList.toggle('backdrop--visible');
+  }
+
+  // ------------------------------
+
+  chooseCity.addEventListener('click', toggleCitySelectionList);
+
+  function toggleCitySelectionList(e) {
+    if (!chooseCity.classList.contains('user-panel__city-selection-title_active')) {
+      citySelectionList.style.maxHeight = citySelectionList.scrollHeight + 'px';
+      chooseCity.classList.add('user-panel__city-selection-title_active');
+      return;
+    }
+
+    citySelectionList.style.maxHeight = 0;
+    chooseCity.classList.remove('user-panel__city-selection-title_active');
+  }
+
+  // -------------------------------
+
+  citySelectionList.addEventListener('click', onChooseCity);
+
+  function onChooseCity(e) {
+    activeCity.textContent = e.target.textContent;
+    toggleCitySelectionList();
   }
 }
