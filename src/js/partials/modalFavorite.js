5639842aafd4b292b3fc26ba10f0cf48c8f5e252
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-10-08 18:57:13 +0300

    add

--- src/js/partials/modalFavorite.js
+++ src/js/partials/modalFavorite.js
@@ -9,14 +9,20 @@ export function modalFavorite() {
 
   console.log(swiperContainer);
   // ---refs--
-  let cardDataFavorite = JSON.parse(localStorage.getItem(KEY_FAVORITE));
-  console.log(cardDataFavorite);
   favoriteBtns.forEach(btn => btn.addEventListener('click', openModalFavorite));
 
   function openModalFavorite(e) {
+    let cardDataFavorite = JSON.parse(localStorage.getItem(KEY_FAVORITE));
     backdrop.classList.add('backdrop-favorite--visible');
     modalWindow.classList.add('modal-favorite--visible');
 
     cardsMarkup(cardDataFavorite, swiperContainer);
   }
+
+  backdrop.addEventListener('click', e => {
+    if (e.target !== backdrop) return;
+
+    backdrop.classList.remove('backdrop-favorite--visible');
+    modalWindow.classList.remove('modal-favorite--visible');
+  });
 }
