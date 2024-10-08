0177203bfbebb8dc78baf45af6a17f853d6a0452
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-10-08 20:26:53 +0300

    add

--- src/js/partials/modalFavorite.js
+++ src/js/partials/modalFavorite.js
@@ -6,23 +6,29 @@ export function modalFavorite() {
   const backdrop = document.querySelector('.js-backdrop-favotite');
   const modalWindow = document.querySelector('.js-modal-favorite');
   const swiperContainer = document.querySelector('.js-swiper-favorite');
-
-  console.log(swiperContainer);
+  const btnClose = document.querySelector('.js-swiper-btn-close');
+  const mobMenu = document.querySelector('.js-mob-menu');
   // ---refs--
+
   favoriteBtns.forEach(btn => btn.addEventListener('click', openModalFavorite));
 
   function openModalFavorite(e) {
     let cardDataFavorite = JSON.parse(localStorage.getItem(KEY_FAVORITE));
     backdrop.classList.add('backdrop-favorite--visible');
     modalWindow.classList.add('modal-favorite--visible');
-
+    document.body.classList.add('to-freeze');
     cardsMarkup(cardDataFavorite, swiperContainer);
   }
 
-  backdrop.addEventListener('click', e => {
-    if (e.target !== backdrop) return;
+  backdrop.addEventListener('click', closeModalWindow);
+  btnClose.addEventListener('click', closeModalWindow);
 
+  function closeModalWindow(e) {
     backdrop.classList.remove('backdrop-favorite--visible');
     modalWindow.classList.remove('modal-favorite--visible');
-  });
+
+    if (mobMenu.classList.contains('mob-menu--visible')) return;
+
+    document.body.classList.remove('to-freeze');
+  }
 }
