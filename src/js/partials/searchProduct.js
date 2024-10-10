8606c856a584ff3bf966f965b40115e5e4177eb7
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-10-10 17:51:45 +0300

    add

--- src/js/partials/searchProduct.js
+++ src/js/partials/searchProduct.js
@@ -1,4 +1,5 @@
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { cardsMarkup } from '../pages/main/cards-markup';
 
 export async function searchProduct(app) {
   console.log();
@@ -33,23 +34,74 @@ export async function searchProduct(app) {
 }
 
 function filterProduct(allDocuments) {
-  // console.log(allDocuments);
-
   const searchInput = document.querySelector('.js-header-search-input');
+  const searchList = document.querySelector('.js-header-search-input-list');
+  const productCatalog = document.querySelector('.js-main-hero-product-catalog');
+  const productCatalogTitle = document.querySelector('.js-main-hero-product-catalog-title');
+  const mainHeroContainer = document.querySelector('.js-main-hero-container');
+  // ---refs-------
 
   searchInput.addEventListener('input', () => {
-    let searchQuery = searchInput.value.trim();
+    let searchQuery = searchInput.value.trim().toLowerCase();
 
-    if (!searchQuery) return;
+    if (!searchQuery) {
+      searchList.innerHTML = '';
+      productCatalogTitle.style.display = 'none';
+      productCatalog.classList.remove('main-hero__product-catalog_active');
+      mainHeroContainer.style.display = 'block';
+      searchList.innerHTML = '';
+      productCatalog.innerHTML = '';
+      return;
+    }
+    productCatalog.classList.add('main-hero__product-catalog_active');
 
-    const filteredСards = allDocuments.filter(({ title }) => title.includes(searchQuery));
-    const filteredСardTitles = filteredСards.map(({ title }) => title);
+    const filteredСards = allDocuments.filter(({ title }) =>
+      title.toLowerCase().includes(searchQuery)
+    );
 
-    console.log(filteredСardTitles);
+    cardsMarkup(filteredСards, productCatalog);
+
+    let markupList = filteredСards.map(({ title }) => {
+      return `
+          <li class="header__search-input-item">
+            <p class="header__search-input-item-title js-search-list-item-title">
+             ${title}
+            </p>
+          </li>`;
+    });
+
+    searchList.innerHTML = [...new Set(markupList)].join('');
+  });
+
+  // ---searchSelectedProduct-----
+  searchList.addEventListener('click', searchSelectedProduct);
+
+  function searchSelectedProduct({ target }) {
+    if (!target.classList.contains('js-search-list-item-title')) return;
+
+    const selectedTitle = target.textContent.trim();
+
+    const selectedСards = allDocuments.filter(
+      ({ title }) => title.toLowerCase() === selectedTitle.toLowerCase()
+    );
+
+    searchInput.value = selectedTitle;
+
+    cardsMarkup(selectedСards, productCatalog);
+    productCatalogTitle.style.display = 'block';
+    mainHeroContainer.style.display = 'none';
+    searchList.innerHTML = '';
+  }
+
+  // ---searchInputBlur-----
+
+  searchInput.addEventListener('blur', () => {
+    setTimeout(() => {
+      searchList.classList.add('header__search-input-list_hidden');
+    }, 0);
+  });
+
+  searchInput.addEventListener('focus', () => {
+    searchList.classList.remove('header__search-input-list_hidden');
   });
 }
-
-// const app = initializeApp(firebaseConfig);
-// searchProduct(app).then(allDocuments => {
-//   console.log(allDocuments); // Тепер тут будуть усі документи з Firestore
-// });
