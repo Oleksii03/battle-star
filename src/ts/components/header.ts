83ddc4136cebeff1bd8449adaa634ba11abdc4e8
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-10-15 20:16:05 +0300

    add

--- src/ts/components/header.ts
+++ src/ts/components/header.ts
@@ -1,8 +1,9 @@
 export function header() {
-  const sergioBox = document.querySelector('.js-sergio-item-box') as HTMLElement;
-  const sergioInnerList = document.querySelector('.js-sergio-inner-list') as HTMLElement;
-  // ---refs----
+  const sergioBox = document.querySelector('.js-sergio-item-box') as HTMLElement | null;
+  const sergioInnerList = document.querySelector('.js-sergio-inner-list') as HTMLElement | null;
+  const sergioActiveItem = document.querySelector('.js-sergio-active-title') as HTMLElement | null;
 
+  // --refs----
   if (sergioBox) {
     sergioBox.addEventListener('click', toggleSergioList);
   } else {
@@ -10,6 +11,37 @@ export function header() {
   }
 
   function toggleSergioList(): void {
-    sergioInnerList.classList.toggle('header__Sergio-inner-list_open');
+    if (sergioInnerList) {
+      sergioInnerList.classList.toggle('header__Sergio-inner-list_open');
+    }
   }
+
+  if (sergioInnerList) {
+    sergioInnerList.addEventListener('click', getTextContent);
+  }
+
+  function getTextContent(event: Event): void {
+    const target = event.target as HTMLElement;
+    if (sergioActiveItem) {
+      sergioActiveItem.textContent = target.textContent;
+    }
+    toggleSergioList();
+  }
+
+  // close all lists---
+  document.addEventListener('click', (e: Event) => {
+    let targetEl = (e.target as HTMLElement).closest(
+      '.header__Sergio-inner-item'
+    ) as HTMLElement | null;
+    let parentTargetEl = (e.target as HTMLElement).closest(
+      '.header__Sergio-item-box'
+    ) as HTMLElement | null;
+    // --refs----
+    if (
+      !targetEl?.classList.contains('header__Sergio-inner-item') &&
+      !parentTargetEl?.classList.contains('header__Sergio-item-box')
+    ) {
+      sergioInnerList?.classList.remove('header__Sergio-inner-list_open');
+    }
+  });
 }
