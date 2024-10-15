bd54323ee34460a187c024d5b55fac0d7d8f17aa
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-10-15 21:02:13 +0300

    Update index.ts

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,7 +1,5 @@
 import { header } from './components/header';
-// import { closeLists } from './components/closeOpenLists';
 
 document.addEventListener('DOMContentLoaded', () => {
   header();
-  // closeLists();
 });
