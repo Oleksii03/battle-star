e9f61ffb38ce6344e3b70eb03e10479bac5ca5e3
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-10-16 20:37:56 +0300

    add-slider

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -5,3 +5,33 @@ document.addEventListener('DOMContentLoaded', () => {
   header();
   closeDropDownList();
 });
+import '@splidejs/splide/css';
+import Splide from '@splidejs/splide';
+
+import { Grid } from '@splidejs/splide-extension-grid';
+
+new Splide('.splide', {
+  gap: '30px',
+  grid: {
+    rows: 2,
+    cols: 3,
+    gap: {
+      row: '30px',
+      col: '30px',
+    },
+  },
+  pagination: false,
+
+  breakpoints: {
+    768: {
+      grid: {
+        cols: 2,
+      },
+    },
+
+    1919: {
+      arrows: false,
+      pagination: true,
+    },
+  },
+}).mount({ Grid });
