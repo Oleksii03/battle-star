538fc31fea1083d30059b0776b266da2ff880c52
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-10-17 17:05:58 +0300

    add-firebase

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,11 +1,13 @@
 import { header } from './components/header';
 import { closeDropDownList } from './components/closeDropDownList';
 import { battleStarNewsSlider } from './components/splideSlider';
+import { firebaseConfig } from './utils/firebaseConfig';
 
 document.addEventListener('DOMContentLoaded', () => {
+  let app = firebaseConfig();
   header();
   closeDropDownList();
   battleStarNewsSlider();
-});
 
-// ------
+  console.log(app);
+});
