62c0457987229119374f2a5377b4657c34034340
Oleksii03 <102686871+Oleksii03@users.noreply.github.com>
2024-10-24 10:31:44 +0000

    Deploying to gh-pages from @ Oleksii03/battle-star@13f99b8d624d63e70acb964b8fdbac98921b0efe 🚀

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
-import{i as v,g as b,a as L,c as w,S as y,G as S}from"../assets/vendor-e19STYO_.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&o(a)}).observe(document,{childList:!0,subtree:!0});function t(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(e){if(e.ep)return;e.ep=!0;const r=t(e);fetch(e.href,r)}})();const l=document.querySelector(".js-sergio-inner-list"),d=document.querySelector(".js-game-dropdown-item-inner-list");function $(){const i=document.querySelector(".js-sergio-item-box"),s=document.querySelector(".js-sergio-active-title"),t=document.querySelector(".js-game-dropdown-box"),o=document.querySelector(".js-game-dropdown-active-title");i==null||i.addEventListener("click",e);function e(){l==null||l.classList.toggle("header__Sergio-inner-list_open")}l==null||l.addEventListener("click",r);function r(c){const n=c.target;s&&n&&(s.textContent=n.textContent),e()}t==null||t.addEventListener("click",a);function a(){d==null||d.classList.toggle("game-dropdown-item__inner-list_open")}d==null||d.addEventListener("click",p);function p(c){const n=c.target;o&&(o.textContent=n.textContent),a()}}function j(){document.addEventListener("click",t=>{i(t),s(t)});function i(t){const o=t.target.closest(".header__Sergio-inner-item"),e=t.target.closest(".header__Sergio-item-box");!(o!=null&&o.classList.contains("header__Sergio-inner-item"))&&!(e!=null&&e.classList.contains("header__Sergio-item-box"))&&(l==null||l.classList.remove("header__Sergio-inner-list_open"))}function s(t){const o=t.target.closest(".game-dropdown-item__inner-item"),e=t.target.closest(".js-game-dropdown-box");!(o!=null&&o.classList.contains("game-dropdown-item__inner-item"))&&!(e!=null&&e.classList.contains("js-game-dropdown-box"))&&(d==null||d.classList.remove("game-dropdown-item__inner-list_open"))}}function O(){return v({apiKey:"AIzaSyAd5LeDQLc4i5UEZgy049Nmu5687cFRTFU",authDomain:"battlestar-514be.firebaseapp.com",projectId:"battlestar-514be",storageBucket:"battlestar-514be.appspot.com",messagingSenderId:"1041425711014",appId:"1:1041425711014:web:b36feae118870e4d2c5967"})}async function u(i){const s=O(),t=b(s);try{const o=await L(w(t,i)),e=[];return o.forEach(r=>{e.push({id:r.id,...r.data()})}),e}catch(o){return console.error("Error getting documents:",o),[]}}function x(i){const s=document.querySelector(".js-main-slider-news");if(!s)return;const t=i.map(o=>{const{title:e,description:r,views:a,date:p,image:{png:c,webp:n},author:{img:f,name:m}}=o;return`
+import{i as v,g as b,a as w,c as L,S as y,G as S}from"../assets/vendor-e19STYO_.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&a(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();const l=document.querySelector(".js-sergio-inner-list"),p=document.querySelector(".js-game-dropdown-item-inner-list");function $(){const i=document.querySelector(".js-sergio-item-box"),t=document.querySelector(".js-sergio-active-title"),s=document.querySelector(".js-game-dropdown-box"),a=document.querySelector(".js-game-dropdown-active-title");i==null||i.addEventListener("click",e);function e(){l==null||l.classList.toggle("header__Sergio-inner-list_open")}l==null||l.addEventListener("click",r);function r(c){const n=c.target;t&&n&&(t.textContent=n.textContent),e()}s==null||s.addEventListener("click",o);function o(){p==null||p.classList.toggle("game-dropdown-item__inner-list_open")}p==null||p.addEventListener("click",d);function d(c){const n=c.target;a&&(a.textContent=n.textContent),o()}}function k(){document.addEventListener("click",s=>{i(s),t(s)});function i(s){const a=s.target.closest(".header__Sergio-inner-item"),e=s.target.closest(".header__Sergio-item-box");!(a!=null&&a.classList.contains("header__Sergio-inner-item"))&&!(e!=null&&e.classList.contains("header__Sergio-item-box"))&&(l==null||l.classList.remove("header__Sergio-inner-list_open"))}function t(s){const a=s.target.closest(".game-dropdown-item__inner-item"),e=s.target.closest(".js-game-dropdown-box");!(a!=null&&a.classList.contains("game-dropdown-item__inner-item"))&&!(e!=null&&e.classList.contains("js-game-dropdown-box"))&&(p==null||p.classList.remove("game-dropdown-item__inner-list_open"))}}function j(){return v({apiKey:"AIzaSyAd5LeDQLc4i5UEZgy049Nmu5687cFRTFU",authDomain:"battlestar-514be.firebaseapp.com",projectId:"battlestar-514be",storageBucket:"battlestar-514be.appspot.com",messagingSenderId:"1041425711014",appId:"1:1041425711014:web:b36feae118870e4d2c5967"})}async function m(i){const t=j(),s=b(t);try{const a=await w(L(s,i)),e=[];return a.forEach(r=>{e.push({id:r.id,...r.data()})}),e}catch(a){return console.error("Error getting documents:",a),[]}}function x(i){const t=document.querySelector(".js-main-slider-news");if(!t)return;const s=i.map(a=>{const{title:e,description:r,views:o,date:d,image:{png:c,webp:n},author:{img:_,name:u}}=a;return`
       <li class="splide__slide">
             <!-- slide-body -->
 
             <div class="splide__slide-body">
+             <a href="#">
               <picture>
                 <source
                   srcset="${n}"
@@ -14,7 +15,7 @@ import{i as v,g as b,a as L,c as w,S as y,G as S}from"../assets/vendor-e19STYO_.
                   src="${c}"
                   alt="${e}" />
               </picture>
-
+             </a>
               <!-- slide-description -->
 
               <div class="splide__slide-description">
@@ -26,15 +27,17 @@ import{i as v,g as b,a as L,c as w,S as y,G as S}from"../assets/vendor-e19STYO_.
 
                 <div class="splide__slide-author slide-author">
                   <div class="slide-author__content-left">
+                   <a href="#">
                     <img
                       width="40"
                       height="40"
                       class="slide-author__img"
-                      src="${f}"
-                      alt="${m}" />
-
-                    <span class="slide-author__name">${m}</span>
-                    <span class="slide-author__date">${p}</span>
+                      src="${_}"
+                      alt="${u}" />
+                   </a>
+                   
+                    <span class="slide-author__name">${u}</span>
+                    <span class="slide-author__date">${d}</span>
                   </div>
 
                   <div class="slide-author__content-right">
@@ -49,13 +52,13 @@ import{i as v,g as b,a as L,c as w,S as y,G as S}from"../assets/vendor-e19STYO_.
                       </svg>
                     </button>
 
-                    <p class="slide-author__views-count">${a}</p>
+                    <p class="slide-author__views-count">${o}</p>
                   </div>
                 </div>
               </div>
             </div>
-          </li>`});s.innerHTML=t.join("")}function T(){u("SliderBattleStarNews").then(s=>x(s))}function q(){setTimeout(()=>{new y(".splide, main-stage__news-slider",{gap:"30px",grid:{rows:2,cols:3,gap:{row:"30px",col:"30px"}},pagination:!1,breakpoints:{768:{grid:{cols:2}},1919:{arrows:!1,pagination:!0}}}).mount({Grid:S})},500)}const _="mainPageListTop",h="mainPageListTopDota";function E(i,s){if(!i.length){s.innerHTML='<li><p class="list-top__empty">No data available</p></li>';return}const t=[...i].sort((o,e)=>e.score-o.score).map(o=>{const{name:e,score:r,id:a,countryFlagImg:{webp:p},avatar:{png:c,webp:n}}=o;return`
-           <li class="list-top__item" data-id="${a}">
+          </li>`});t.innerHTML=s.join("")}function O(){m("SliderBattleStarNews").then(t=>x(t))}function T(){setTimeout(()=>{new y(".splide, main-stage__news-slider",{gap:"30px",grid:{rows:2,cols:3,gap:{row:"30px",col:"30px"}},pagination:!1,breakpoints:{768:{grid:{cols:2}},1919:{arrows:!1,pagination:!0}}}).mount({Grid:S})},500)}const h="mainPageListTop",f="mainPageListTopDota",q="mainPageLeaderboardCards";function E(i,t){if(!i.length){t.innerHTML='<li><p class="list-top__empty">No data available</p></li>';return}const s=[...i].sort((a,e)=>e.score-a.score).map(a=>{const{name:e,score:r,id:o,countryFlagImg:{webp:d},avatar:{png:c,webp:n}}=a;return`
+           <li class="list-top__item" data-id="${o}">
             <p class="list-top__item-num"></p>
             <picture>
               <source
@@ -75,13 +78,13 @@ import{i as v,g as b,a as L,c as w,S as y,G as S}from"../assets/vendor-e19STYO_.
               width="21"
               height="14"
               class="list-top__svg"
-              src="${p}"
+              src="${d}"
               alt="ua" />
 
             <p class="list-top__name">${e}</p>
             <p class="list-top__score">${r}</p>
-          </li>`});s.innerHTML=t.join("")}function k(i,s){if(!i.length){s.innerHTML='<li><p class="list-top__empty">No data available</p></li>';return}const t=[...i].sort((o,e)=>e.score-o.score).map(o=>{const{name:e,score:r,id:a,countryFlagImg:{webp:p},avatar:{png:c,webp:n}}=o;return`
-           <li class="list-top__item" data-id="${a}">
+          </li>`});t.innerHTML=s.join("")}function M(i,t){if(!i.length){t.innerHTML='<li><p class="list-top__empty">No data available</p></li>';return}const s=[...i].sort((a,e)=>e.score-a.score).map(a=>{const{name:e,score:r,id:o,countryFlagImg:{webp:d},avatar:{png:c,webp:n}}=a;return`
+           <li class="list-top__item" data-id="${o}">
             <p class="list-top__item-num"></p>
             <picture>
               <source
@@ -101,10 +104,69 @@ import{i as v,g as b,a as L,c as w,S as y,G as S}from"../assets/vendor-e19STYO_.
               width="21"
               height="14"
               class="list-top__svg"
-              src="${p}"
+              src="${d}"
               alt="ua" />
 
             <p class="list-top__name">${e}</p>
             <p class="list-top__score">${r}</p>
-          </li>`});s.innerHTML=t.join("")}function g(i=" "){const s=document.querySelector(".js-main-leaderboard-top-list");if(!s){console.error("Leaderboard container not found");return}if(i===_){u(i).then(t=>E(t,s)).catch(t=>console.error("Error fetching CS:GO data:",t));return}if(i===h){u(i).then(t=>k(t,s)).catch(t=>console.error("Error fetching Dota data:",t));return}console.log("Invalid search query")}function M(){const i=document.querySelector(".js-leaderboard-btn-csgo"),s=document.querySelector(".js-leaderboard-btn-dota"),t=document.querySelector(".js-btns-thumb");i==null||i.addEventListener("click",o);function o(){t==null||t.classList.remove("main-leaderboard__btns-thumb_active"),i==null||i.classList.add("csgo-active"),s==null||s.classList.remove("dota-active"),g(_)}o(),s==null||s.addEventListener("click",e);function e(){t==null||t.classList.add("main-leaderboard__btns-thumb_active"),s==null||s.classList.add("dota-active"),i==null||i.classList.remove("csgo-active"),g(h)}}document.addEventListener("DOMContentLoaded",()=>{$(),j(),T(),q(),M()});
+          </li>`});t.innerHTML=s.join("")}function g(i=" "){const t=document.querySelector(".js-main-leaderboard-top-list");if(!t){console.error("Leaderboard container not found");return}if(i===h){m(i).then(s=>E(s,t)).catch(s=>console.error("Error fetching CS:GO data:",s));return}if(i===f){m(i).then(s=>M(s,t)).catch(s=>console.error("Error fetching Dota data:",s));return}console.log("Invalid search query")}function D(i){const t=document.querySelector(".js-main-leaderboard-cards-list");if(!i.length){t.innerHTML='<li><p class="list-top__empty">No data available</p></li>';return}const s=i.map(a=>{const{title:e,date:r,description:o,id:d,img:{png:c,webp:n},likes:_,views:u}=a;return`
+          <li class="main-leaderboard__cards-item" data-id="${d}">
+            <a
+              href="#"
+              class="main-leaderboard__cards-link"
+              aria-label="${e}">
+   
+              <picture>
+                <source
+                  srcset="${n}"
+                  type="image/webp" />
+
+                <img
+                  loading="lazy"
+                  width="248"
+                  height="143"
+                  class="main-leaderboard__cards-img"
+                  src="${c}"
+                  alt="${e}" />
+              </picture>
+            </a>
+
+            <!-- cards-description -->
+
+            <div class="main-leaderboard__cards-info">
+              <h3 class="main-leaderboard__cards-title">
+                ${e}
+              </h3>
+
+              <p class="main-leaderboard__cards-description">
+                ${o}
+              </p>
+
+              <div class="main-leaderboard__cards-stats">
+                <p class="main-leaderboard__cards-stats-date">${r}</p>
+
+                <div class="main-leaderboard__cards-stat">
+                  <div class="main-leaderboard__cards-stat-like">
+                    <svg
+                      width="20"
+                      height="20">
+                      <use xlink:href="#icon-like"></use>
+                    </svg>
+
+                    <p class="main-leaderboard__cards-stat-like-number">${_}</p>
+                  </div>
+                  <!-- views -->
+                  <div class="main-leaderboard__cards-stat-views">
+                    <svg
+                      width="20"
+                      height="20">
+                      <use xlink:href="#icon-eye-views"></use>
+                    </svg>
+
+                    <p class="main-leaderboard__cards-stat-views-number">${u}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>`});t.innerHTML=s.join("")}function C(i=" "){m(i).then(t=>D(t)).catch(t=>console.error("Error fetching data:",t))}function A(){const i=document.querySelector(".js-leaderboard-btn-csgo"),t=document.querySelector(".js-leaderboard-btn-dota"),s=document.querySelector(".js-btns-thumb");i==null||i.addEventListener("click",a);function a(){s==null||s.classList.remove("main-leaderboard__btns-thumb_active"),i==null||i.classList.add("csgo-active"),t==null||t.classList.remove("dota-active"),g(h)}a(),t==null||t.addEventListener("click",e);function e(){s==null||s.classList.add("main-leaderboard__btns-thumb_active"),t==null||t.classList.add("dota-active"),i==null||i.classList.remove("csgo-active"),g(f)}C(q)}document.addEventListener("DOMContentLoaded",()=>{$(),k(),O(),T(),A()});
 //# sourceMappingURL=index.js.map
