17697f03d4b976d0c4c8f6efa3119671f0691c9d
Oleksii03 <102686871+Oleksii03@users.noreply.github.com>
2024-10-29 17:11:15 +0000

    Deploying to gh-pages from @ Oleksii03/battle-star@d4ec390466a416ff424e00395de9f04f5cdf23b1 🚀

--- js/sign-in/sign-in.js
+++ js/sign-in/sign-in.js
@@ -1,2 +1,129 @@
-import"../../assets/modulepreload-polyfill-B5Qt9EMX.js";function s(e){console.log(e)}function a(e){console.log(e)}function i(){const e=document.querySelector(".js-form-btn-enter"),t=document.querySelector(".js-form-btn-create"),o=document.querySelector(".js-form-btns-tumb"),n=document.querySelector(".js-sign-in-form");console.log(e,t),e==null||e.addEventListener("click",c);function c(){e==null||e.classList.add("active-btn"),t==null||t.classList.remove("active-btn"),o==null||o.classList.remove("sign-in__form-btns-tumb_active"),s(n)}c(),t==null||t.addEventListener("click",r);function r(){t==null||t.classList.add("active-btn"),e==null||e.classList.remove("active-btn"),o==null||o.classList.add("sign-in__form-btns-tumb_active"),a(n)}}document.addEventListener("DOMContentLoaded",()=>{i()});
+import"../../assets/modulepreload-polyfill-B5Qt9EMX.js";function r(){return`
+        <div class="sign-in__form-input__wrapper">
+          <div class="sign-in__form-input-box">
+            <input
+              type="email"
+              class="sign-in__form-input"
+              placeholder="Електронна пошта"
+              required />
+          </div>
+
+          <div class="sign-in__form-input-box">
+            <input
+              type="password"
+              class="sign-in__form-input"
+              placeholder="Пароль"
+              required />
+
+            <div class="sign-in__form-input-icon-box">
+              <svg
+                width="24"
+                height="24"
+                class="sign-in__form-input-icon">
+                <use xlink:href="#icon-eye-views"></use>
+              </svg>
+              <span class="sign-in__form-input-icon-line"></span>
+            </div>
+          </div>
+        </div>
+
+        <div class="sign-in__form-restore">
+          <a
+            href="#"
+            class="sign-in__form-forgot-password-link">
+            Забули пароль?
+          </a>
+          <a
+            href="#"
+            class="sign-in__form-restore-link">
+            Відновити
+          </a>
+        </div>
+
+        <button
+          class="sign-in__form-btn-submit"
+          type="submit">
+          Увійти
+        </button>
+
+        <div class="sign-in__log-in-via-title-box">
+          <p class="sign-in__log-in-via-title">Або увійдіть через</p>
+        </div>
+
+        <ul class="sign-in__log-in-via-list">
+          <li class="sign-in__log-in-via-item">
+            <svg
+              width="44"
+              height="44"
+              class="sign-in__log-in-via-icon">
+              <use xlink:href="#icon-form_steam"></use>
+            </svg>
+          </li>
+
+          <li class="sign-in__log-in-via-item">
+            <svg
+              width="44"
+              height="44"
+              class="sign-in__log-in-via-icon">
+              <use xlink:href="#icon-form-apple"></use>
+            </svg>
+          </li>
+
+          <li class="sign-in__log-in-via-item">
+            <svg
+              width="44"
+              height="44"
+              class="sign-in__log-in-via-icon">
+              <use xlink:href="#icon-form_google"></use>
+            </svg>
+          </li>
+
+          <li class="sign-in__log-in-via-item">
+            <svg
+              width="44"
+              height="44"
+              class="sign-in__log-in-via-icon">
+              <use xlink:href="#icon-form-facebook"></use>
+            </svg>
+          </li>
+        </ul>`}function l(i){i.innerHTML=r()}function a(){return`
+        <div class="sign-in__form-input-box">
+          <input
+            type="text"
+            class="sign-in__form-input"
+            placeholder="Введіть нікнейм"
+            required />
+        </div>
+
+        <div class="sign-in__form-input-box">
+          <input
+            type="email"
+            class="sign-in__form-input"
+            placeholder="Електронна пошта"
+            required />
+        </div>
+
+        <div class="sign-in__form-input-box">
+          <input
+            type="password"
+            class="sign-in__form-input"
+            placeholder="Придумайте пароль"
+            required />
+
+          <div class="sign-in__form-input-icon-box">
+            <svg
+              width="24"
+              height="24"
+              class="sign-in__form-input-icon">
+              <use xlink:href="#icon-eye-views"></use>
+            </svg>
+            <span class="sign-in__form-input-icon-line"></span>
+          </div>
+        </div>
+
+        <button
+          class="sign-in__form-btn-submit sign-in__form-btn-submit_create"
+          type="submit">
+          Створити
+        </button>`}function c(i){i.innerHTML=a()}function _(){const i=document.querySelector(".js-form-btn-enter"),n=document.querySelector(".js-form-btn-create"),s=document.querySelector(".js-form-btns-tumb"),e=document.querySelector(".js-sign-in-form");i==null||i.addEventListener("click",t);function t(){i==null||i.classList.add("active-btn"),n==null||n.classList.remove("active-btn"),s==null||s.classList.remove("sign-in__form-btns-tumb_active"),e instanceof HTMLFormElement&&l(e)}t(),n==null||n.addEventListener("click",o);function o(){n==null||n.classList.add("active-btn"),i==null||i.classList.remove("active-btn"),s==null||s.classList.add("sign-in__form-btns-tumb_active"),e instanceof HTMLFormElement&&c(e)}}document.addEventListener("DOMContentLoaded",()=>{_()});
 //# sourceMappingURL=sign-in.js.map
