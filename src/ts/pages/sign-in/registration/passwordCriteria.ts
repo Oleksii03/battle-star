a2a12ed0f4592e5d2851b616f8fca4283f13fde3
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-10-30 19:12:36 +0200

    add

--- src/ts/pages/sign-in/registration/passwordCriteria.ts
+++ src/ts/pages/sign-in/registration/passwordCriteria.ts
@@ -5,11 +5,10 @@ interface PasswordCriteria {
   minNumbers: number;
   minSymbols: number;
 }
-
 export const passwordCriteria: PasswordCriteria = {
   minLength: 8,
   minLowercase: 1,
   minUppercase: 1,
   minNumbers: 1,
-  minSymbols: 1,
+  minSymbols: 0,
 };
--- src/ts/pages/sign-in/registration/registerUser.ts
+++ src/ts/pages/sign-in/registration/registerUser.ts
@@ -9,21 +9,25 @@ export async function registerUser(
   password: string,
   nickname: string
 ): Promise<User> {
+  console.log(passwordCriteria);
   // Валідація email та пароля
-
   if (!isEmail(email)) {
     // throw new Error('Invalid email address');
     console.log('Invalid email address');
+    return;
   }
   if (!isStrongPassword(password, passwordCriteria)) {
     // throw new Error('Password is not strong enough');
     console.log('Password is not strong enough');
+    return;
   }
 
   try {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
     const user = userCredential.user;
     await updateProfile(user, { displayName: nickname });
+
+    window.location.href = 'cabinet.html';
     return user;
   } catch (error: any) {
     console.error('Error creating user:', error.code, error.message);
