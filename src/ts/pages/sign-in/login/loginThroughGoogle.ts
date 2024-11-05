a93cecd2fc7ceac4b86a8bcc1e1f293f86ef129b
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-11-05 14:33:01 +0200

    Update loginThroughGoogle.ts

--- src/ts/pages/sign-in/login/loginThroughGoogle.ts
+++ src/ts/pages/sign-in/login/loginThroughGoogle.ts
@@ -1,8 +1,6 @@
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { auth } from '@/ts/utils/firebaseConfig';
-import { baseUrl } from '@/ts/index';
-// import { cabinetUrl } from '@/ts/cabinet';
-// import { cabinetUrl } from '@/ts/utils/router';
+import { createCabinetUrl } from '@/ts/utils/router';
 
 export function loginThroughGoogle() {
   const provider = new GoogleAuthProvider();
@@ -13,7 +11,7 @@ export function loginThroughGoogle() {
 
       console.log('User signed in with Google:', user);
 
-      // window.location.href = cabinetUrl;
+      window.location.href = createCabinetUrl();
     })
     .catch(error => {
       console.error('Error signing in with Google:', error);
