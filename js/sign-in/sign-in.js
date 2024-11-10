4791b55760a9765288daa6066612e89508d71a6c
Oleksii03 <102686871+Oleksii03@users.noreply.github.com>
2024-11-10 18:29:51 +0000

    Deploying to gh-pages from @ Oleksii03/battle-star@ee8e7b925895134c0874ffa72d671c63500249c0 🚀

--- js/sign-in/sign-in.js
+++ js/sign-in/sign-in.js
@@ -1,2 +1,2 @@
-import{o as n}from"../../assets/cabinet-3eao7NQ2.js";import{s as e}from"../../assets/loginOrRegistrationHandler-Bh4-AkiO.js";import"../../assets/vendor-DZUFF-U2.js";document.addEventListener("DOMContentLoaded",()=>{e(),n()});
+import{o as n}from"../../assets/cabinet-SgzsRjSu.js";import{s as e}from"../../assets/loginOrRegistrationHandler-uHCgcz5p.js";import"../../assets/vendor-DZUFF-U2.js";document.addEventListener("DOMContentLoaded",()=>{e(),n()});
 //# sourceMappingURL=sign-in.js.map
