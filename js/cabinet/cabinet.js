bdacd3e3da7eb646217f3c913ebcff21b8da59cc
Oleksii03 <102686871+Oleksii03@users.noreply.github.com>
2024-11-13 18:35:26 +0000

    Deploying to gh-pages from @ Oleksii03/battle-star@aa1f398c8a0225129770f7284740430166f0c7f5 🚀

--- js/cabinet/cabinet.js
+++ js/cabinet/cabinet.js
@@ -1,4 +1,4 @@
-import{h as k,o as u}from"../../assets/cabinet-CWERuWV8.js";import{P as f,g as w,H as x,h as j,l as L,c as S}from"../../assets/closeDropDownList-D3JACSJ9.js";import{s as q}from"../../assets/loginOrRegistrationHandler-CBIEuIa6.js";import"../../assets/vendor-DZUFF-U2.js";function T(){if(window.location.pathname.includes(f)){const i=document.querySelector(".js-user-inner-list"),t=document.querySelector(".js-sidebar-btn-exit"),a=document.querySelector(".js-sidebar-btn-enter"),e=document.querySelector(".js-user-active-title"),c=document.querySelector(".js-icon-arrow-down"),r=document.querySelector(".header__user-item");t==null||t.classList.remove("sidebar__nav-item_hidden"),a==null||a.classList.add("sidebar__nav-item_hidden"),e.style.cursor="auto",e&&(e.style.cursor="auto"),i&&(i.style.display="none"),c&&(c.style.display="none"),r&&(r.style.cursor="auto")}}function y(s,i){const t=document.querySelector(".js-cabinet-nav-thumb"),a=[...i.children],e=a.indexOf(s);t.style.top=`${16.66*e}%`,a.forEach(c=>{c.classList.remove("cabinet__nav-item_active")}),s.classList.add("cabinet__nav-item_active")}function g(){return`
+import{h as k,o as u}from"../../assets/cabinet-CWERuWV8.js";import{P as f,g as x,H as w,h as j,l as L,c as S}from"../../assets/closeDropDownList-D3JACSJ9.js";import{s as q}from"../../assets/loginOrRegistrationHandler-CBIEuIa6.js";import"../../assets/vendor-DZUFF-U2.js";function T(){if(window.location.pathname.includes(f)){const i=document.querySelector(".js-user-inner-list"),t=document.querySelector(".js-sidebar-btn-exit"),a=document.querySelector(".js-sidebar-btn-enter"),e=document.querySelector(".js-user-active-title"),c=document.querySelector(".js-icon-arrow-down"),r=document.querySelector(".header__user-item");t==null||t.classList.remove("sidebar__nav-item_hidden"),a==null||a.classList.add("sidebar__nav-item_hidden"),e.style.cursor="auto",e&&(e.style.cursor="auto"),i&&(i.style.display="none"),c&&(c.style.display="none"),r&&(r.style.cursor="auto")}}function g(s,i){const t=document.querySelector(".js-cabinet-nav-thumb"),a=[...i.children],e=a.indexOf(s);t.style.top=`${16.66*e}%`,a.forEach(c=>{c.classList.remove("cabinet__nav-item_active")}),s.classList.add("cabinet__nav-item_active")}function h(){return`
           <div class="cabinet-statistics__rank">
             <p class="cabinet-statistics__rank-title">
               Ранг:
@@ -120,113 +120,196 @@ import{h as k,o as u}from"../../assets/cabinet-CWERuWV8.js";import{P as f,g as w
               </picture>
             </div>
           </div>`}function M(){return`
-          <div class="cabinet-statistics__rank">
-            <p class="cabinet-statistics__rank-title">
-              Ранг:
-              <span>1682</span>
+            <div class="cabinet-statistics__rank">
+          <p class="cabinet-statistics__rank-title">
+            Ранг:
+            <span>1682</span>
 
-                <img
-                  width="63"
-                  height="30"
-                  loading="lazy"
-                  class="cabinet-statistics__rank-title-icon"
-                  src="../../assets/img/pages/cabinet/statistics/rank-icon-scgo.svg"
-                  alt="rank icon scgo" />
-            </p>
+            <img
+              width="63"
+              height="30"
+              loading="lazy"
+              class="cabinet-statistics__rank-title-icon"
+              src="../../assets/img/pages/cabinet/statistics/rank-icon-scgo.svg"
+              alt="rank icon scgo" />
+          </p>
 
-            <ul class="cabinet-statistics__rank-list">
-              <li class="cabinet-statistics__rank-item">
-                <svg
-                  width="26"
-                  height="26"
-                  class="cabinet-statistics__rank-item-icon cabinet-statistics__rank-item-icon_tournaments">
-                  <use xlink:href="#icon-sidebar-tournaments"></use>
-                </svg>
+          <ul class="cabinet-statistics__rank-list">
+            <li class="cabinet-statistics__rank-item">
+              <svg
+                width="26"
+                height="26"
+                class="cabinet-statistics__rank-item-icon cabinet-statistics__rank-item-icon_tournaments">
+                <use xlink:href="#icon-sidebar-tournaments"></use>
+              </svg>
 
-                <div class="cabinet-statistics__rank-item-desc">
-                  <p class="cabinet-statistics__rank-item-value">4</p>
-                  <p class="cabinet-statistics__rank-item-text">Турніру</p>
-                </div>
+              <div class="cabinet-statistics__rank-item-desc">
+                <p class="cabinet-statistics__rank-item-value">4</p>
+                <p class="cabinet-statistics__rank-item-text">Турніру</p>
+              </div>
+            </li>
+
+            <li class="cabinet-statistics__rank-item">
+              <svg
+                width="26"
+                height="26"
+                class="cabinet-statistics__rank-item-icon cabinet-statistics__rank-item-icon_statistics">
+                <use xlink:href="#icon-statistics"></use>
+              </svg>
+
+              <div class="cabinet-statistics__rank-item-desc">
+                <p class="cabinet-statistics__rank-item-value">76%</p>
+                <p class="cabinet-statistics__rank-item-text">Вінрейт</p>
+              </div>
+            </li>
+
+            <li class="cabinet-statistics__rank-item">
+              <svg
+                width="26"
+                height="26"
+                class="cabinet-statistics__rank-item-icon cabinet-statistics__rank-item-icon_clock">
+                <use xlink:href="#icon-clock"></use>
+              </svg>
+
+              <div class="cabinet-statistics__rank-item-desc">
+                <p class="cabinet-statistics__rank-item-value">214</p>
+                <p class="cabinet-statistics__rank-item-text">Годинник у грі</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+
+        <div class="cabinet-statistics__game-style game-style">
+          <div class="game-style__game-box">
+            <p class="game-style__title">Стиль гри</p>
+
+            <ul class="game-style__list">
+              <li class="game-style__list-item">
+                <ul class="game-style__inner-list">
+                  <li class="game-style__inner-list-item">
+                    <p class="game-style__inner-list-item-text">Матчів зіграно:</p>
+                    <p class="game-style__inner-list-item-score">1023</p>
+                  </li>
+                  <li class="game-style__inner-list-item">
+                    <p class="game-style__inner-list-item-text">Карт зіграно:</p>
+                    <p class="game-style__inner-list-item-score">23</p>
+                  </li>
+                </ul>
               </li>
 
-              <li class="cabinet-statistics__rank-item">
-                <svg
-                  width="26"
-                  height="26"
-                  class="cabinet-statistics__rank-item-icon cabinet-statistics__rank-item-icon_statistics">
-                  <use xlink:href="#icon-statistics"></use>
-                </svg>
-
-                <div class="cabinet-statistics__rank-item-desc">
-                  <p class="cabinet-statistics__rank-item-value">76%</p>
-                  <p class="cabinet-statistics__rank-item-text">Вінрейт</p>
-                </div>
+              <li class="game-style__list-item">
+                <ul class="game-style__inner-list">
+                  <li class="game-style__inner-list-item">
+                    <p class="game-style__inner-list-item-text">Перемог:</p>
+                    <p class="game-style__inner-list-item-score">798</p>
+                  </li>
+                  <li class="game-style__inner-list-item">
+                    <p class="game-style__inner-list-item-text">Програшів:</p>
+                    <p class="game-style__inner-list-item-score">225</p>
+                  </li>
+                </ul>
               </li>
 
-              <li class="cabinet-statistics__rank-item">
-                <svg
-                  width="26"
-                  height="26"
-                  class="cabinet-statistics__rank-item-icon cabinet-statistics__rank-item-icon_clock">
-                  <use xlink:href="#icon-clock"></use>
-                </svg>
-
-                <div class="cabinet-statistics__rank-item-desc">
-                  <p class="cabinet-statistics__rank-item-value">214</p>
-                  <p class="cabinet-statistics__rank-item-text">Годинник у грі</p>
-                </div>
+              <li class="game-style__list-item">
+                <ul class="game-style__inner-list">
+                  <li class="game-style__inner-list-item">
+                    <p class="game-style__inner-list-item-text">Вбивств:</p>
+                    <p class="game-style__inner-list-item-score">2304</p>
+                  </li>
+                  <li class="game-style__inner-list-item">
+                    <p class="game-style__inner-list-item-text">Смерть:</p>
+                    <p class="game-style__inner-list-item-score">1389</p>
+                  </li>
+                </ul>
               </li>
             </ul>
           </div>
 
-    
-          <div class="cabinet-statistics__game-style game-style">
-            <div class="game-style__game-box">
-              <p class="game-style__title">Стиль гри</p>
+          <div class="game-style__bar-chart bar-chart">
+            <p class="bar-chart__title">Стиль гри</p>
 
-              <ul class="game-style__list">
-                <li class="game-style__list-item">
-                  <ul class="game-style__inner-list">
-                    <li class="game-style__inner-list-item">
-                      <p class="game-style__inner-list-item-text">Матчів зіграно:</p>
-                      <p class="game-style__inner-list-item-score">1023</p>
-                    </li>
-                    <li class="game-style__inner-list-item">
-                      <p class="game-style__inner-list-item-text">Карт зіграно:</p>
-                      <p class="game-style__inner-list-item-score">23</p>
-                    </li>
-                  </ul>
+            <div class="bar-chart__bar-box">
+              <ul class="bar-chart__list-bg">
+                <li class="bar-chart__list-bg-item">
+                  <p class="percent">100</p>
+                  <div class="line"></div>
+                  <p class="percent">100</p>
                 </li>
 
-                <li class="game-style__list-item">
-                  <ul class="game-style__inner-list">
-                    <li class="game-style__inner-list-item">
-                      <p class="game-style__inner-list-item-text">Перемог:</p>
-                      <p class="game-style__inner-list-item-score">798</p>
-                    </li>
-                    <li class="game-style__inner-list-item">
-                      <p class="game-style__inner-list-item-text">Програшів:</p>
-                      <p class="game-style__inner-list-item-score">225</p>
-                    </li>
-                  </ul>
+                <li class="bar-chart__list-bg-item">
+                  <p class="percent">75</p>
+                  <div class="line"></div>
+                  <p class="percent">75</p>
                 </li>
 
-                <li class="game-style__list-item">
-                  <ul class="game-style__inner-list">
-                    <li class="game-style__inner-list-item">
-                      <p class="game-style__inner-list-item-text">Вбивств:</p>
-                      <p class="game-style__inner-list-item-score">2304</p>
-                    </li>
-                    <li class="game-style__inner-list-item">
-                      <p class="game-style__inner-list-item-text">Смерть:</p>
-                      <p class="game-style__inner-list-item-score">1389</p>
-                    </li>
-                  </ul>
+                <li class="bar-chart__list-bg-item">
+                  <p class="percent">50</p>
+                  <div class="line"></div>
+                  <p class="percent">50</p>
+                </li>
+
+                <li class="bar-chart__list-bg-item">
+                  <p class="percent">25</p>
+                  <div class="line"></div>
+                  <p class="percent">25</p>
+                </li>
+
+                <li class="bar-chart__list-bg-item">
+                  <p class="percent">0</p>
+                  <div class="line"></div>
+                  <p class="percent">0</p>
                 </li>
               </ul>
-            </div>
 
-          </div>`}function C(s,i){const t=document.querySelector(".js-cabinet-statistics-thumb");s.innerHTML=g(),i.addEventListener("click",a=>{const e=a.target;if(e.tagName==="BUTTON"){if([...i.children].forEach(c=>c.classList.remove("cabinet-statistics__btn_active")),e.classList.contains("js-statistics-btn-csgo")){s.innerHTML=M(),t==null||t.classList.add("cabinet-statistics__btns-thumb_active"),e.classList.add("cabinet-statistics__btn_active");return}if(e.classList.contains("js-statistics-btn-dota")){s.innerHTML=g(),t==null||t.classList.remove("cabinet-statistics__btns-thumb_active"),e.classList.add("cabinet-statistics__btn_active");return}}})}function E(s){const i=`
+              <div class="bar-chart__indicators-box">
+                <ul class="bar-chart__indicators-list">
+                  <li class="bar-chart__indicators-item">
+                    <div class="performance-indicators-track">
+                      <div
+                        class="performance-indicators-track-thumb performance-indicators-track-thumb_precision"></div>
+                    </div>
+                    <p class="bar-chart__indicators-item-text">Точність, %</p>
+                  </li>
+                  <li class="bar-chart__indicators-item">
+                    <div class="performance-indicators-track">
+                      <div
+                        class="performance-indicators-track-thumb performance-indicators-track-thumb_hitting-head"></div>
+                    </div>
+                    <p class="bar-chart__indicators-item-text">Попадання в голову, %</p>
+                  </li>
+                  <li class="bar-chart__indicators-item">
+                    <div class="performance-indicators-track">
+                      <div
+                        class="performance-indicators-track-thumb performance-indicators-track-thumb_vitality"></div>
+                    </div>
+                    <p class="bar-chart__indicators-item-text">Живучість, %</p>
+                  </li>
+                  <li class="bar-chart__indicators-item">
+                    <div class="performance-indicators-track">
+                      <div
+                        class="performance-indicators-track-thumb performance-indicators-track-thumb_play-time"></div>
+                    </div>
+                    <p class="bar-chart__indicators-item-text">Час гри</p>
+                  </li>
+                  <li class="bar-chart__indicators-item">
+                    <div class="performance-indicators-track">
+                      <div
+                        class="performance-indicators-track-thumb performance-indicators-track-thumb_weapon-efficiency"></div>
+                    </div>
+                    <p class="bar-chart__indicators-item-text">Ефективність зброї, %</p>
+                  </li>
+                  <li class="bar-chart__indicators-item">
+                    <div class="performance-indicators-track">
+                      <div
+                        class="performance-indicators-track-thumb performance-indicators-track-thumb_assists"></div>
+                    </div>
+                    <p class="bar-chart__indicators-item-text">Кількість асистів, %</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>`}function C(s,i){const t=document.querySelector(".js-cabinet-statistics-thumb");s.innerHTML=h(),i.addEventListener("click",a=>{const e=a.target;if(e.tagName==="BUTTON"){if([...i.children].forEach(c=>c.classList.remove("cabinet-statistics__btn_active")),e.classList.contains("js-statistics-btn-csgo")){s.innerHTML=M(),t==null||t.classList.add("cabinet-statistics__btns-thumb_active"),e.classList.add("cabinet-statistics__btn_active");return}if(e.classList.contains("js-statistics-btn-dota")){s.innerHTML=h(),t==null||t.classList.remove("cabinet-statistics__btns-thumb_active"),e.classList.add("cabinet-statistics__btn_active");return}}})}function E(s){const i=`
         <div class="cabinet-statistics">
           <div class="cabinet-statistics__btns">
             <div class="cabinet-statistics__btns-box js-statistics-btns-box">
@@ -328,5 +411,5 @@ import{h as k,o as u}from"../../assets/cabinet-CWERuWV8.js";import{P as f,g as w
           </div>
 
           <ul class="cabinet-history__list js-cabinet-history-list"></ul>
-        </div>`;s.innerHTML=i,w(x).then(t=>{o(t),H(t)}).catch(k)}function h(s,i){$(s);const t=i.dataset.hash;t&&(location.hash=t)}function D(s,i){const t=location.hash.slice(1)?location.hash.slice(1):"statistics",a=document.querySelector(`[data-hash="${t}"]`);switch(y(a,i),t){case"statistics":b(s,a);break;case"history":h(s,a);break;default:b(s,a);break}}function I(){const s=document.querySelector(".js-cabinet-nav-list"),i=document.querySelector(".js-cabinet-main-body");s==null||s.addEventListener("click",t);function t(a){const e=a.target.closest("li");e&&(y(e,s),e.classList.contains("js-statistics")&&b(i,e),e.classList.contains("js-history")&&h(i,e))}D(i,s)}function O(){const s=localStorage.getItem("logged");(s==="false"||s===null)&&(window.location.href="404")}document.addEventListener("DOMContentLoaded",()=>{T(),j(),q(),u(),u(),L(),I(),O(),S()});
+        </div>`;s.innerHTML=i,x(w).then(t=>{o(t),H(t)}).catch(k)}function y(s,i){$(s);const t=i.dataset.hash;t&&(location.hash=t)}function D(s,i){const t=location.hash.slice(1)?location.hash.slice(1):"statistics",a=document.querySelector(`[data-hash="${t}"]`);switch(g(a,i),t){case"statistics":b(s,a);break;case"history":y(s,a);break;default:b(s,a);break}}function I(){const s=document.querySelector(".js-cabinet-nav-list"),i=document.querySelector(".js-cabinet-main-body");s==null||s.addEventListener("click",t);function t(a){const e=a.target.closest("li");e&&(g(e,s),e.classList.contains("js-statistics")&&b(i,e),e.classList.contains("js-history")&&y(i,e))}D(i,s)}function O(){const s=localStorage.getItem("logged");(s==="false"||s===null)&&(window.location.href="404")}document.addEventListener("DOMContentLoaded",()=>{T(),j(),q(),u(),u(),L(),I(),O(),S()});
 //# sourceMappingURL=cabinet.js.map
