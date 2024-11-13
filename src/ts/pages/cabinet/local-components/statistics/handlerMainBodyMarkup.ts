d677df4ba39f8d6f41e5af961ffd47a2237f85e2
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-11-13 16:23:58 +0200

    add

--- src/ts/pages/cabinet/local-components/statistics/handlerMainBodyMarkup.ts
+++ src/ts/pages/cabinet/local-components/statistics/handlerMainBodyMarkup.ts
@@ -1,6 +1,10 @@
 import { createMainBodyMarkupDota } from './createMainBodyMarkupDota';
+import { createMainBodyMarkupCsgo } from './createMainBodyMarkupCsgo';
 
 export function handlerMainBodyMarkup(contentContainer: HTMLDivElement, btnsBox: HTMLDivElement) {
+  const thumb = document.querySelector('.js-cabinet-statistics-thumb');
+  // ---/-refs--------
+
   // default-markup-handler
   contentContainer.innerHTML = createMainBodyMarkupDota();
 
@@ -10,14 +14,22 @@ export function handlerMainBodyMarkup(contentContainer: HTMLDivElement, btnsBox:
 
     if (targetEl.tagName !== 'BUTTON') return;
 
+    [...btnsBox.children].forEach(btn => btn.classList.remove('cabinet-statistics__btn_active'));
+
     // handler-markups
     if (targetEl.classList.contains('js-statistics-btn-csgo')) {
-      console.log(targetEl);
+      contentContainer.innerHTML = createMainBodyMarkupCsgo();
+
+      thumb?.classList.add('cabinet-statistics__btns-thumb_active');
+      targetEl.classList.add('cabinet-statistics__btn_active');
       return;
     }
 
     if (targetEl.classList.contains('js-statistics-btn-dota')) {
       contentContainer.innerHTML = createMainBodyMarkupDota();
+
+      thumb?.classList.remove('cabinet-statistics__btns-thumb_active');
+      targetEl.classList.add('cabinet-statistics__btn_active');
       return;
     }
   });
