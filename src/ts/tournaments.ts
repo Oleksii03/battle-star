4e8d7be4db62fab8e742c8bcbd000b3c55128b0e
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-11-25 14:25:08 +0200

    add

--- src/ts/tournaments.ts
+++ src/ts/tournaments.ts
@@ -4,10 +4,22 @@ import { onStateUserChanged } from './pages/sign-in/cabinet/cabinet';
 import { logoutThroughSidebar } from './pages/sign-in/cabinet/signOut';
 import { settingsHtmlComponents } from './utils/settingsHtmlComponents';
 
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Ukrainian } from 'flatpickr/dist/l10n/uk.js';
+
 document.addEventListener('DOMContentLoaded', () => {
   header();
   closeDropDownList();
   onStateUserChanged();
   logoutThroughSidebar();
   settingsHtmlComponents();
+
+  // Ініціалізація календаря
+  flatpickr('#myCalendar', {
+    locale: Ukrainian,
+    inline: true,
+    dateFormat: 'Y-m-d',
+    mode: 'range',
+  });
 });
