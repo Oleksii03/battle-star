b4c750a19d659a09dc9b3865ee92172243a19151
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2024-11-28 12:36:34 +0200

    add

--- src/ts/pages/tournaments/handlerRankTrack.ts
+++ src/ts/pages/tournaments/handlerRankTrack.ts
@@ -4,9 +4,6 @@ export const rankList = document.querySelector('.js-rank-track-list')!;
 // ---reusable-refs---`
 
 export function handlerRankTrack() {
-  const rankTrackTumb = document.querySelector('.js-rank-track-tumb') as HTMLDivElement;
-  // ---local-refs ---
-
   rankList?.addEventListener('click', handlerTrackList);
 
   function handlerTrackList(e: Event) {
@@ -14,25 +11,6 @@ export function handlerRankTrack() {
 
     if (targetLi?.tagName !== 'LI') return;
 
-    //---handlers---
-    if (targetLi.classList.contains('js-rank-item-1')) {
-      rankTrackTumb.style.width = 4 + '%';
-    }
-    if (targetLi.classList.contains('js-rank-item-2')) {
-      rankTrackTumb.style.width = 27 + '%';
-      destinationHandler(targetLi);
-    }
-    if (targetLi.classList.contains('js-rank-item-3')) {
-      rankTrackTumb.style.width = 50 + '%';
-      destinationHandler(targetLi);
-    }
-    if (targetLi.classList.contains('js-rank-item-4')) {
-      rankTrackTumb.style.width = 73 + '%';
-      destinationHandler(targetLi);
-    }
-    if (targetLi.classList.contains('js-rank-item-5')) {
-      rankTrackTumb.style.width = 100 + '%';
-      destinationHandler(targetLi);
-    }
+    destinationHandler(targetLi);
   }
 }
--- src/ts/pages/tournaments/handlerRankTrackItem.ts
+++ src/ts/pages/tournaments/handlerRankTrackItem.ts
@@ -4,6 +4,7 @@ export function destinationHandler(targetLi: HTMLLIElement) {
   const inputs = document.querySelectorAll('.js-rank-item-input');
   const idx = findIdx(targetLi);
   setActiveInputsColor(idx, inputs);
+  setScaleProgress(idx);
 }
 
 function findIdx(targetEl: HTMLElement) {
@@ -15,5 +16,29 @@ function setActiveInputsColor(idx: number, inputs: NodeListOf<Element>) {
   for (let i = 0; i < idx; i++) {
     const input = inputs[i];
     input.classList.add('form__rank-item-input_active');
+  }
+}
+
+function setScaleProgress(idx: number) {
+  const rankTrackTumb = document.querySelector('.js-rank-track-tumb') as HTMLDivElement;
+
+  switch (idx) {
+    case 0:
+      rankTrackTumb.style.width = 4 + '%';
+      break;
+    case 1:
+      rankTrackTumb.style.width = 27 + '%';
+      break;
+    case 2:
+      rankTrackTumb.style.width = 50 + '%';
+      break;
+    case 3:
+      rankTrackTumb.style.width = 73 + '%';
+      break;
+    case 4:
+      rankTrackTumb.style.width = 100 + '%';
+      break;
+    default:
+      rankTrackTumb.style.width = 4 + '%';
   }
 }
