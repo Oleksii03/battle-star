2505c613b5d19958a627f76ff67e1c59a66151a3
Oleksii03 <102686871+Oleksii03@users.noreply.github.com>
2024-11-28 16:16:27 +0000

    Deploying to gh-pages from @ Oleksii03/battle-star@754ae2157904c63f54127c5a79c5aa3add565284 🚀

---
+++ assets/getDataFromFirestore-B98LJQAD.js
@@ -0,0 +1,2 @@
+import{g as s,a as c,c as n}from"./vendor-ZgN2xaTy.js";import{a as i}from"./cabinet-6ap2872v.js";async function m(a){const e=s(i);try{const r=await c(n(e,a)),t=[];return r.forEach(o=>{t.push({id:o.id,...o.data()})}),t}catch(r){return console.error("Error getting documents:",r),[]}}export{m as g};
+//# sourceMappingURL=getDataFromFirestore-B98LJQAD.js.map
--- js/sign-in/sign-in.js
+++ js/sign-in/sign-in.js
@@ -1,2 +1,2 @@
-import{o as n}from"../../assets/cabinet-DeI5YiPA.js";import{s as e}from"../../assets/loginOrRegistrationHandler-Bf-39iK6.js";import"../../assets/vendor-ZgN2xaTy.js";document.addEventListener("DOMContentLoaded",()=>{e(),n()});
+import{o as n}from"../../assets/cabinet-6ap2872v.js";import{s as e}from"../../assets/loginOrRegistrationHandler-CxMgT3o5.js";import"../../assets/vendor-ZgN2xaTy.js";document.addEventListener("DOMContentLoaded",()=>{e(),n()});
 //# sourceMappingURL=sign-in.js.map
